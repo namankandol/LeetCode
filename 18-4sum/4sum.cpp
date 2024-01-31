f116e7b86aaf92d1fd7f3dad20d53df5aca4770b
Naman Kandol <82331362+namankandol@users.noreply.github.com>
2024-01-31 16:07:05 +0530

    Time: 18 ms (79.86%) | Memory: 15.8 MB (35.89%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int tar) {
         int n = nums.size();
-        set<vector<int>> st;
-
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> v;
         for(int i=0; i<n; i++){
+            if(i>0 and nums[i] == nums[i-1]) continue;
             for(int j=i+1; j<n; j++){
-                set<long long> fst;
-                for(int k=j+1; k<n; k++){
-                    long long sum = nums[i] + nums[j];
+                if(j>i+1 and nums[j] == nums[j-1]) continue;
+                int k = j+1, l = n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum += nums[j];
                     sum += nums[k];
-                    long long need = tar - sum;
-                    if(fst.find(need) != fst.end()){
-                        vector<int> sub = {
-                            nums[i], nums[j], nums[k], (int)(need)};
-                        sort(sub.begin(), sub.end());
-                        st.insert(sub);
+                    sum += nums[l];
+                    if(sum == tar){
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        v.push_back(temp);
+                        k++;
+                        l--;
+                        while(k<l and nums[k] == nums[k-1]) k++;
+                        while(k<l and nums[l] == nums[l+1]) l--;
                     }
-                    fst.insert(nums[k]);
-                }
+                    else if(sum > tar) l--;
+                    else k++;
+                }  
             }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
-        return ans;
+        return v;
     }
 };
